3c6f429d9eadddd0a27b44ee516c7bf5ab51207b
Rohan <rohan@Rohans-MacBook-Pro.local>
2017-08-23 21:32:24 +0530

    Bug fixes

--- example/script.js
+++ example/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$("#file").change(function(e){
 		var img = e.target.files[0];
 
-		if(!iEdit.open(img, true, function(res){
+		if(!pixelarity.open(img, true, function(res){
 			$("#result").attr("src", res);			
 		})){
 			alert("Whoops! That is not an image!");
